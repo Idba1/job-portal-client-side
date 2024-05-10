cc5af2721922ce176f817e411460bd8dc88687b0
Idba1 <moniraislamself@gmail.com>
2024-05-10 18:24:34 +0600

    set project and install npm package

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className='text-4xl font-slab'>Hello</h1>
   </React.StrictMode>,
 )
