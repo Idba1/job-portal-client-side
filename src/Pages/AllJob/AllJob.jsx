cdd0dbfee6b9184f0a2b1c0e0664fbac7696593d
Idba1 <moniraislamself@gmail.com>
2024-05-11 15:41:21 +0600

    All Jobs table/list and Implement a search system on this page, based on the Job Title

--- src/Pages/AllJob/AllJob.jsx
+++ src/Pages/AllJob/AllJob.jsx
@@ -1,62 +1,56 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const AllJob = () => {
+
     const alljob = useLoaderData();
     console.log(alljob);
+
+    const [searchTittle, setSearchTittle] = useState('');
+
+    const filteredJobs = alljob.filter(job =>
+        job.title.toLowerCase().includes(searchTittle.toLowerCase())
+    );
+
+
     return (
-        <div className="grid grid-cols-1 mx-auto gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3 p-6">
-            <div className=" rounded-md shadow-md bg-gray-950 dark:text-white">
-               
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                    <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Web Development</h2>
-                        <p className="dark:text-white">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                    </div>
-                    <Link to={"/job-details"}>
-                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details Button
-                        </button>
-                    </Link>
-                </div>
+
+        <div className="mx-auto w-2/3 my-14">
+            <div className="mb-10">
+                <h1 className="">If you locking any specific job! Search here...</h1>
+                <input
+                    className="block w-2/3 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    type="text"
+                    placeholder="Search by Job Title"
+                    value={searchTittle}
+                    onChange={(e) => setSearchTittle(e.target.value)}
+                />
             </div>
-            <div className=" rounded-md shadow-md bg-gray-950 dark:text-white">
-               
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                    <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Web Development</h2>
-                        <p className="dark:text-white">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                    </div>
-                    <Link to={"/job-details"}>
-                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details Button
-                        </button>
-                    </Link>
-                </div>
-            </div>
-            <div className=" rounded-md shadow-md bg-gray-950 dark:text-white">
-               
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                    <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Web Development</h2>
-                        <p className="dark:text-white">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                    </div>
-                    <Link to={"/job-details"}>
-                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details Button
-                        </button>
-                    </Link>
-                </div>
-            </div>
-            <div className=" rounded-md shadow-md bg-gray-950 dark:text-white">
-               
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                    <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Web Development</h2>
-                        <p className="dark:text-white">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                    </div>
-                    <Link to={"/job-details"}>
-                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Details Button
-                        </button>
-                    </Link>
-                </div>
-            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Job Title</th>
+                        <th>Job Posting Date</th>
+                        <th>Application Deadline</th>
+                        <th>Salary Range</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredJobs.map(job => (
+                        <tr key={job.id}>
+                            <td>{job.title}</td>
+                            <td>{job.postingDate}</td>
+                            <td>{job.deadline}</td>
+                            <td>{job.salaryRange}</td>
+                            <td>
+                                <Link to={"/job-details"}> <button className="btn">View Details</button></Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
