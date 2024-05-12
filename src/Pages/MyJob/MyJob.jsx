692118e715db480115f7359eb4e936f2101069aa
Idba1 <moniraislamself@gmail.com>
2024-05-12 17:54:36 +0600

    fatch and display all my added job

--- src/Pages/MyJob/MyJob.jsx
+++ src/Pages/MyJob/MyJob.jsx
@@ -1,11 +1,31 @@
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider"
+import axios from "axios"
+
 const MyJob = () => {
+    const { user } = useContext(AuthContext)
+    const [jobs, setJobs] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(
+                `${import.meta.env.VITE_API_URL}/jobs/${user?.email}`
+            )
+            setJobs(data)
+        }
+        getData()
+    }, [user])
+    console.log(jobs);
+
+
+    
     return (
         <section className='container px-4 mx-auto pt-12'>
             <div className='flex items-center gap-x-3'>
-                <h2 className='text-lg font-medium text-gray-800 '>My Bids</h2>
+                <h2 className='text-lg font-medium text-gray-800 '>My Posted Jobs</h2>
 
                 <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
-                    05 Bid
+                    {jobs.length} Jobs
                 </span>
             </div>
 
@@ -37,7 +57,7 @@ const MyJob = () => {
                                             className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
                                         >
                                             <button className='flex items-center gap-x-2'>
-                                                <span>Price</span>
+                                                <span>Price Range</span>
                                             </button>
                                         </th>
 
@@ -47,69 +67,90 @@ const MyJob = () => {
                                         >
                                             Category
                                         </th>
-
                                         <th
                                             scope='col'
                                             className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
                                         >
-                                            Status
+                                            Description
                                         </th>
 
                                         <th className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'>
-                                            Actions
+                                            Edit
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody className='bg-white divide-y divide-gray-200 '>
-                                    <tr>
-                                        <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                                            Build Dynamic Website
-                                        </td>
 
-                                        <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                                            10/04/2024
-                                        </td>
+                                    {
+                                        jobs.map(job => (
+                                            <tr key={job._id}>
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.title}
+                                                </td>
 
-                                        <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                                            $200
-                                        </td>
-                                        <td className='px-4 py-4 text-sm whitespace-nowrap'>
-                                            <div className='flex items-center gap-x-2'>
-                                                <p
-                                                    className='px-3 py-1 rounded-full text-blue-500 bg-blue-100/60 text-xs'
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.deadline}
+                                                </td>
+
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.salaryRange}
+                                                </td>
+                                                <td className='px-4 py-4 text-sm whitespace-nowrap'>
+                                                    <div className='flex items-center gap-x-2'>
+                                                        <p
+                                                            className='px-3 py-1 rounded-full text-blue-500 bg-blue-100/60
+                             text-xs'
+                                                        >
+                                                            {job.category}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                                <td
+                                                    title=''
+                                                    className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'
                                                 >
-                                                    Web Development
-                                                </p>
-                                            </div>
-                                        </td>
-                                        <td className='px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap'>
-                                            <div className='inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-yellow-100/60 text-yellow-500'>
-                                                <span className='h-1.5 w-1.5 rounded-full bg-yellow-500'></span>
-                                                <h2 className='text-sm font-normal '>Pending</h2>
-                                            </div>
-                                        </td>
-                                        <td className='px-4 py-4 text-sm whitespace-nowrap'>
-                                            <button
-                                                title='Mark Complete'
-                                                className='text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none disabled:cursor-not-allowed'
-                                            >
-                                                <svg
-                                                    xmlns='http://www.w3.org/2000/svg'
-                                                    fill='none'
-                                                    viewBox='0 0 24 24'
-                                                    strokeWidth='1.5'
-                                                    stroke='currentColor'
-                                                    className='w-5 h-5'
-                                                >
-                                                    <path
-                                                        strokeLinecap='round'
-                                                        strokeLinejoin='round'
-                                                        d='M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75'
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                                   {job.description}
+                                                </td>
+                                                <td className='px-4 py-4 text-sm whitespace-nowrap'>
+                                                    <div className='flex items-center gap-x-6'>
+                                                        <button className='text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none'>
+                                                            <svg
+                                                                xmlns='http://www.w3.org/2000/svg'
+                                                                fill='none'
+                                                                viewBox='0 0 24 24'
+                                                                strokeWidth='1.5'
+                                                                stroke='currentColor'
+                                                                className='w-5 h-5'
+                                                            >
+                                                                <path
+                                                                    strokeLinecap='round'
+                                                                    strokeLinejoin='round'
+                                                                    d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                                                                />
+                                                            </svg>
+                                                        </button>
+
+                                                        <button className='text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none'>
+                                                            <svg
+                                                                xmlns='http://www.w3.org/2000/svg'
+                                                                fill='none'
+                                                                viewBox='0 0 24 24'
+                                                                strokeWidth='1.5'
+                                                                stroke='currentColor'
+                                                                className='w-5 h-5'
+                                                            >
+                                                                <path
+                                                                    strokeLinecap='round'
+                                                                    strokeLinejoin='round'
+                                                                    d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
+                                                                />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
                                 </tbody>
                             </table>
                         </div>
