7ae4792d847235d1d2810a548a01f2193e13a8bb
Idba1 <moniraislamself@gmail.com>
2024-05-12 07:55:06 +0600

    set date picker

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -1,9 +1,13 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../../Provider/AuthProvider/AuthProvider"
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 const AddJob = () => {
-    const {user} = useContext(AuthContext)
-    const email =user?.email || 'user@email.com';
+    const [startDate, setStartDate] = useState(new Date())
+
+    const { user } = useContext(AuthContext)
+    const email = user?.email || 'user@email.com';
     const name = user?.displayName || 'user';
 
 
@@ -88,7 +92,7 @@ const AddJob = () => {
 
                         <div>
                             <label className='text-gray-700 ' htmlFor='postingDate'>
-                            postingDate
+                                postingDate
                             </label>
                             <input
                                 id='postingDate'
@@ -134,17 +138,23 @@ const AddJob = () => {
                             />
                         </div>
 
-                        
+
                         <div className='flex flex-col gap-2 '>
                             <label className='text-gray-700'>Deadline</label>
 
                             {/* Date Picker Input Field */}
+                            <DatePicker
+                                className='border p-2 rounded-md'
+                                selected={startDate}
+                                onChange={date => setStartDate(date)}
+                            />
                         </div>
+
                     </div>
 
                     <div className='flex flex-col gap-2 mt-4'>
                         <label className='text-gray-700 ' htmlFor='job-description'>
-                           Job Description
+                            Job Description
                         </label>
                         <textarea
                             className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
