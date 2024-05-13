b728d226cfcbd923554e1cd83b84bf09c169b0fd
Idba1 <moniraislamself@gmail.com>
2024-05-13 13:09:50 +0600

    applied job

--- src/Pages/AppliedJob/AppliedJob.jsx
+++ src/Pages/AppliedJob/AppliedJob.jsx
@@ -1,10 +1,93 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
+import axios from "axios";
 
 const AppliedJob = () => {
+    const { user } = useContext(AuthContext)
+    const [jobs, setJobs] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(
+                `${import.meta.env.VITE_API_URL}/apply/${user?.email}`
+            )
+            setJobs(data)
+        }
+        getData()
+    }, [user])
+    console.log(jobs);
+
+
     return (
-        <div>
-            
-        </div>
-    );
+        <section className='container px-4 mx-auto pt-12'>
+            <div className='flex items-center gap-x-3'>
+                <h2 className='text-lg font-medium text-gray-800 '>My Bids</h2>
+
+                <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
+                    {jobs.length}
+                </span>
+            </div>
+
+            <div className='flex flex-col mt-6'>
+                <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                    <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+                        <div className='overflow-hidden border border-gray-200  md:rounded-lg'>
+                            <table className='min-w-full divide-y divide-gray-200'>
+                                <thead className='bg-gray-50'>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            <div className='flex items-center gap-x-3'>
+                                                <span>Name:</span>
+                                            </div>
+                                        </th>
+
+                                        <th
+                                            scope='col'
+                                            className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            <span>Email:</span>
+                                        </th>
+
+
+                                        <th
+                                            scope='col'
+                                            className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            Resume Link
+                                        </th>
+
+                                    </tr>
+                                </thead>
+                                <tbody className='bg-white divide-y divide-gray-200 '>
+                                    {
+                                        jobs.map(job => (
+                                            <tr key={job._id}>
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.name}
+                                                </td>
+
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.email}
+                                                </td>
+
+                                                <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                    {job.resumeLink}
+                                                </td>
+
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </section >
+    )
 };
 
 export default AppliedJob;
