1ae3685f328083af4e704d6ae8297a119c695aad
Idba1 <moniraislamself@gmail.com>
2024-05-15 17:12:25 +0600

    error page update

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,22 +1,34 @@
-import { Link } from "react-router-dom";
-
 const ErrorPage = () => {
     return (
-        <div>
-        <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
-            <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-                <div className="max-w-md text-center">
-                    <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-                        <span className="sr-only">Error</span>404
-                    </h2>
-                    <p className="text-2xl font-semibold md:text-3xl">Oops! Page Not Found</p>
-                    <p className="mt-4 mb-8 dark:text-gray-600">We are sorry, but the page you are looking for could not be found. It may have been removed, had its name changed, or is temporarily unavailable. Please check the URL for any errors or return to the homepage.</p>
-                    <Link href="/"> <button className="btn text-white bg-green-600">Home </button></Link>
-                </div>
+      <section className='bg-white '>
+        <div className='container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-5'>
+          <div className='wf-ull lg:w-1/2'>
+            <p className='text-4xl font-medium text-gray- '>404 error</p>
+            <h1 className='mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl'>
+              Page not found
+            </h1>
+            <p className=' text-gray-500 dark:text-gray-400'>
+              Sorry, the page you are looking for doesnt exist.Here are some
+              helpful links:
+            </p>
+  
+            <div className='flex items-center mt-6 gap-x-3'>
+              <button className='px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-sky-600 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600'>
+                Take me home
+              </button>
             </div>
-        </section>
-    </div>
-    );
-};
-
-export default ErrorPage;
+          </div>
+  
+          <div className='relative w-full mt-8 lg:w-1/2 lg:mt-0'>
+            <img
+              className=' w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover '
+              src='https://i.ibb.co/ZG9KHnZ/error-404-6052476-640.png'
+              alt=''
+            />
+          </div>
+        </div>
+      </section>
+    )
+  }
+  
+  export default ErrorPage
