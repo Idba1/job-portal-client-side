488377222ef68c961336cce4b6213200c19ac091
Idba1 <moniraislamself@gmail.com>
2024-11-06 19:06:34 +0600

    update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
     }, []);
 
     return (
-        <div className="z-50">
+        <div className="z-50 bg-white">
             <nav className={isSticky ? 'navbar fixed' : 'navbar' }>
                 <div className="navbar z-10  shadow-lgn">
                     <div className="navbar-start">
@@ -50,6 +50,7 @@ const Navbar = () => {
                             <div tabIndex={0} role="button" className=" btn btn-ghost lg:hidden">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </div>
+                          
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 <NavLink className={({ isActive }) =>
                                 isActive ? 'text-[#0077B5]' : theme === 'dark' ? 'text-[#FFFFFF]' : 'text-[#333333]' } to={"/"}>Home</NavLink>
@@ -95,11 +96,11 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="navbar-end gap-1 lg:gap-3 md:gap-3">
-                        <label className="cursor-pointer grid place-items-center">
+                        {/* <label className="cursor-pointer grid place-items-center">
                             <input type="checkbox" onChange={handleTheme} value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                             <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                             <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                        </label>
+                        </label> */}
 
                         <div className='flex'>
 
@@ -133,14 +134,14 @@ const Navbar = () => {
                                     :
                                     <NavLink className="btn text-white  lg:font-bold bg-[#0E46A3]" to={"/login"}>LogIn</NavLink>
                             }
-                            {
+                            {/* {
                                 user ? <div>
                                     <NavLink className="btn hidden text-white  lg:font-bold bg-[#0E46A3]" to={"/register"}>Register</NavLink>
                                 </div> :
                                     <div>
                                         <NavLink className="btn text-white visible lg:font-bold bg-[#0E46A3]" to={"/register"}>Register</NavLink>
                                     </div>
-                            }
+                            } */}
 
                         </div>
 
